2c48b8c3399570fab2ec8d23f8d60594b1fd5568
Adrian Tokle Storset <55131770+ATS-Hackerman@users.noreply.github.com>
2021-05-11 02:17:48 +0200

    :sparkles: Add list for marks and setup for win checks

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,11 +1,12 @@
 package tictactoe;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
+
+    static ArrayList<Integer> player_positions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpu_positions = new ArrayList<Integer>();
+
     public static void main(String[] args) {
         char[][] game_board = {{' ', '|', ' ', '|', ' '},
                               {'-', '+', '-', '+', '-'},
@@ -82,7 +83,7 @@ public class TicTacToe {
         }
     }
 
-    public static String checkWinner(){
+    public static String checkWinner() {
         List top_row = Arrays.asList(1, 2, 3);
         List middle_row = Arrays.asList(4, 5, 6);
         List bottom_row = Arrays.asList(7, 8, 9);
@@ -93,6 +94,21 @@ public class TicTacToe {
 
         List diagonal1 = Arrays.asList(1, 5, 9);
         List diagonal2 = Arrays.asList(7, 5, 3);
+
+        List<List> win_conditions = new ArrayList<List>();
+        win_conditions.add(top_row);
+        win_conditions.add(middle_row);
+        win_conditions.add(bottom_row);
+        win_conditions.add(left_column);
+        win_conditions.add(middle_column);
+        win_conditions.add(right_column);
+        win_conditions.add(diagonal1);
+        win_conditions.add(diagonal2);
+
+        for(List l : win_conditions) {
+
+        }
+
         return "";
     }
 }
